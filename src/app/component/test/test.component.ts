14c6ed20aae390d4ca7331a844594c9a5bc5be9d
muriveinick <muroveinick148@gmailc.com>
2021-02-17 20:47:28 +0300

    13th

--- src/app/component/test/test.component.ts
+++ src/app/component/test/test.component.ts
@@ -13,25 +13,30 @@ export class TestComponent implements OnInit {
     document.querySelectorAll('circle').forEach((c, index) => {
       c.setAttribute('r', `${this.r}`);
       c.setAttribute('stroke-dasharray', `${(this.r * 2 * Math.PI).toFixed(2)}`)
-      if (index > 0) c.setAttribute('stroke-dashoffset', `${(this.r * 2 * Math.PI).toFixed(2)}`)
+      if (index % 2 != 0) c.setAttribute('stroke-dashoffset', `${(this.r * 2 * Math.PI).toFixed(2)}`)
     })
 
-
+    document.querySelectorAll('mat-icon.hero-icon').forEach(el => el.setAttribute("style", `right:${this.r * 9 / 4}px;`))
   }
-  @Input() r: number = 70;
+  @Input() r: number = 40;
   level: number = 0;
 
-  setProgressBar(a: number) {
-
-    let bar = document.querySelector("#bar")
-    // bar.setAttribute('stroke-dashoffset', `${this.r * 2 * Math.PI * (11 - a) / 11}`);
-    bar.setAttribute('style', `stroke-dashoffset: ${this.r * 2 * Math.PI * (11 - a) / 11}px`)
+  setProgressBar(relicLevel: number) {
+    let bar = document.querySelector(".bar")
+    bar.setAttribute('style', `stroke-dashoffset: ${this.r * 2 * Math.PI * (11 - relicLevel) / 11}px`)
   }
 
-  onChangeLevel(bool) {
-    bool ? this.level < 11 ? this.level++ : null : this.level > 0 ? this.level-- : null
 
+  onChangeLevel(bool: boolean) {
+    bool ? this.level < 11 ? this.level++ : null : this.level > 0 ? this.level-- : null
     return this.setProgressBar(this.level)
+  }
+
+
+  onChange(a) {
+    let k = a.path.indexOf('div.aaa')
+    console.log(a.path.indexOf('div.aaa'));
+    console.log(a.path["div.aaa"].attributes.name.nodeValue);
   }
 
 }
