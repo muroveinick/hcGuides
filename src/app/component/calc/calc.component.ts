6c8b4b6d64579bc7a3627a7d41b9bdeb6ba3b8ba
muriveinick <muroveinick148@gmailc.com>
2021-02-20 16:14:03 +0300

    20th

--- src/app/component/calc/calc.component.ts
+++ src/app/component/calc/calc.component.ts
@@ -10,51 +10,37 @@ import { data } from './data';
 export class CalcComponent implements OnInit {
   constructor() { }
   data: any;
+  res: any = [];
 
   default = {
-    banner: 0,
-    hunters: 0,
-    multiply: 1,
     isStartedSouls: false,
   };
 
   ngOnInit(): void {
     this.data = data;
 
-    // console.log(this.data.hunters || 'asda');
     this.form.setValue({
       portalLevel: 80,
       goalSouls: 70000,
-      startedSouls: 0
+      startedSouls: 0,
+      hunters: 1,
+      banner: 1,
+      multiply: 1
     });
+
+    this.form.valueChanges.subscribe((r) => this.showResult())
+
+    this.showResult()
   }
 
   form = new FormGroup({
-    portalLevel: new FormControl('', [
-      Validators.min(1),
-      Validators.max(125),
-      Validators.pattern(/^\d{1,3}$/),
-    ]),
-    goalSouls: new FormControl('', [
-      Validators.min(100),
-      Validators.max(1250000000),
-      Validators.pattern(/\d{1,}/),
-    ]),
-    startedSouls: new FormControl('', [
-      Validators.min(0),
-      Validators.max(1250000000),
-      Validators.pattern(/\d{1,}/),
-    ]),
+    portalLevel: new FormControl('', [Validators.min(1), Validators.max(125),]),
+    goalSouls: new FormControl('', [Validators.min(100), Validators.max(1250000000), Validators.pattern(/\d{1,}/),]),
+    startedSouls: new FormControl('', [Validators.min(0), Validators.max(1250000000), Validators.pattern(/\d{1,}/),]),
+    hunters: new FormControl('', []),
+    banner: new FormControl('', []),
+    multiply: new FormControl('', [])
   });
-
-  checkkValid(level: any) {
-    // console.log(
-    //   `${!!level} +" wqw"+ ${typeof +level === 'number'} +" "+  ${
-    //     level <= 125
-    //   }+"   "+  ${level > 0}`
-    // );
-    return !!level && typeof +level === 'number' && level <= 125 && level > 0;
-  }
 
   showResult() {
     let requiredApples: number;
@@ -66,23 +52,29 @@ export class CalcComponent implements OnInit {
           (finalData.goalSouls -
             (finalData.startedSouls && this.default.isStartedSouls ? finalData.startedSouls : 0)) /
           (this.data.souls[`${finalData.portalLevel}`] *
-            this.default.multiply *
-            (this.default.banner ? this.default.banner : 1) *
-            (this.default.hunters ? this.default.hunters : 1))
+            (finalData.banner ? finalData.banner : 1) *
+            (finalData.hunters ? finalData.hunters : 1)
+            + this.data.souls[`${finalData.portalLevel}`] * (finalData.multiply - 1))
         ) * this.data.apples[`${finalData.portalLevel}`];
 
       if (finalData.startedSouls > finalData.goalSouls) {
-        return "Уже хватает😀"
+        this.res = ["Уже хватает😀"];
+        return
       }
-    }
+      if (!finalData.portalLevel) {
+        return
+      }
 
-    return this.form.valid && finalData.portalLevel
-      ? [`Яблок на ${finalData.portalLevel < 81 ? "Тёмные души" : "Очень тёмные души"}`,
+      this.res = [`Яблок на ${finalData.portalLevel < 81 ? "Тёмные души" : "Очень тёмные души"}`,
       this.formatToSepString(requiredApples),
         "потребуется",
       Math.trunc(requiredApples / this.data.apples[`${finalData.portalLevel}`]),
         "заходов"]
-      : '¯\\_(ツ)_/¯';
+    } else {
+      this.res = ['¯\\_(ツ)_/¯'];
+    }
+
+
   }
 
   formatToSepString(number: number) {
