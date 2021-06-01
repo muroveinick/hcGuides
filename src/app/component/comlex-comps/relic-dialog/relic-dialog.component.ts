3cdc3cdac65b5af88ab6ab615bdd08a83fae9bed
muroveinick <muroveinick148@gmail.com>
2021-06-01 04:19:15 +0300

    m

--- src/app/component/comlex-comps/relic-dialog/relic-dialog.component.ts
+++ src/app/component/comlex-comps/relic-dialog/relic-dialog.component.ts
@@ -18,14 +18,11 @@ export class RelicDialog {
     public translate: TranslateService
   ) { }
 
-  title = this.translate.instant('description.' + this.RelicData.description)
+  title: string = "";
 
 
   ngOnInit() {
-    this.translate.instant('description.' + this.RelicData.description).match(/{}/g).forEach((element, index) => {
-      this.title.replace(element, this.RelicData.variables[index][this.RelicData.curr_level])
-    });
-    console.log(this.title)
+    this.changeDesc()
   }
 
   onNoClick(): void {
@@ -34,11 +31,25 @@ export class RelicDialog {
 
 
   onChangeLevel(value: number) {
+    console.log(this.RelicData)
     if (typeof value === "number") {
       value < 0 ? value = 0 : value > this.RelicData.type.levels ? value = this.RelicData.type.levels : null
       this.RelicData.curr_level = value;
-      relics[this.RelicData.logo.match(/warrior/) ? 0 : this.RelicData.logo.match(/mage/) ? 1 : 2].data[this.RelicData.id].curr_level = value
+      // relics[this.RelicData.logo.match(/warrior/) ? 0 : this.RelicData.logo.match(/mage/) ? 1 : 2].data[this.RelicData.id].curr_level = value
     }
+    this.changeDesc();
   }
 
+  changeDesc() {
+    let new_title = this.translate.instant('description.' + this.RelicData.description);
+    let reg = new_title.match(/{}/g);
+    // console.log(reg)
+    reg.forEach((element: string, index: number) => {
+      // console.log(element, index, this.RelicData.curr_level)
+      // console.log(this.RelicData.variables[index][this.RelicData.curr_level - 1].toString())
+    new_title = new_title.replace(element, (this.RelicData.variables ? this.RelicData.variables[index][this.RelicData.curr_level - 1] : '{}') ?? 0)
+    });
+    this.title = new_title;
+    // console.log(this.title);
+  }
 }
